137d5e071ed7fc6d9aea24ef19f208abd925e80a
Gurpreet <bhangu_gurpreet@yahoo.com>
2020-10-20 17:15:30 -0400

    first commit

---
+++ Git_Uploading_test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Git_Uploading_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone");
+            Console.WriteLine("Just checking how to add dotnet project to github");
+            Console.WriteLine("Thank You for your help");
+        }
+    }
+}
